61fd68cec521d72e890a7dceabe974f983322600
AidenLindsey <aidenlindsey0628@gmail.com>
2024-11-01 15:52:16 -0500

    need to see something rq

--- problem 4/intToBin.py
+++ problem 4/intToBin.py
@@ -3,19 +3,28 @@ def int_to_reverse_binary(num1):
 #write your while loop here
     while num1 > 0:
         output = num1 % 2
-
-    return binary_val;
+        binary_val= binary_val + f'{output:.0f}'
+        num1 =num1 // 2
+    
+    return binary_val
 
 
 def string_reverse(input_string): 
     reverse_input = ''
-    
+    length = len(int_to_reverse_binary(num1))
+    spot = length - 1
+    empty = [0]*length
    #write your for loop here
+    for i in int_to_reverse_binary(num1):
+        empty[spot] = i
+        spot -= 1
+    reverse_input = ''.join(empty)
+    return reverse_input
+if __name__ == '__main__':
+    print('Enter your number: ')
+    num1 = int(input())
     
-
-    user_input = int(input())
-    
-    binary_string = str(int_to_reverse_binary(user_input))
+    binary_string = str(int_to_reverse_binary(num1))
     binary_string = str(string_reverse(binary_string))
     
     print(binary_string)
