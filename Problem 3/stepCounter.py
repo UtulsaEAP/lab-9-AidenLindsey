46639c9a3bef419f364505503e198d448d7069b6
AidenLindsey <aidenlindsey0628@gmail.com>
2024-11-01 15:23:16 -0500

    2 and 3 anwered

--- Problem 3/stepCounter.py
+++ Problem 3/stepCounter.py
@@ -1,10 +1,13 @@
 def feet_to_steps(user_feet):
    #write your code here
-   return
+   steps = user_feet / 2.5
+   return(int(steps))
 
 if __name__ == '__main__':
     #take input feet steps here
     #store it into the function
-    
+    print('Enter the number of feet you walked')
+    user_feet = float(input())
     #print your steps here
+    print(feet_to_steps(user_feet))
     feet_to_steps(5280)
