356dadf70b92c7e1025d9630cb412075692b7d03
AidenLindsey <aidenlindsey0628@gmail.com>
2024-11-05 09:34:37 -0600

    Finished the lab

--- problem 5/varSwap.py
+++ problem 5/varSwap.py
@@ -1,7 +1,10 @@
 def swap_values(user_val1, user_val2, user_val3, user_val4):   
    #write your code here
-   
-   return user_val1, user_val2, user_val3, user_val4
+   user_input1 = user_val2
+   user_input2 = user_val1
+   user_input3 = user_val4
+   user_input4 = user_val3
+   return user_input1, user_input2, user_input3, user_input4
 
 if __name__ == '__main__':   
    user_input1 = int(input())
@@ -10,5 +13,5 @@ if __name__ == '__main__':
    user_input4 = int(input())
    #store output for every input here
    #print those output
-
- 
+   user_input1, user_input2, user_input3, user_input4 = swap_values(user_input1, user_input2, user_input3, user_input4)
+   print(user_input1, user_input2, user_input3, user_input4)
--- problem 6/fib.py
+++ problem 6/fib.py
@@ -1,7 +1,20 @@
 def fibonacci(n):
-    
+    a=0
+    b=1
+    if n < 0:
+       ans = -1
+    elif n == 0:
+       ans = 0
+    elif n ==1:
+        ans = 1
+    else:
+       for i in range(n-1): 
+            c = a + b
+            a= b
+            b = c
+       ans = c  
     #write your code here
-    return
+    return ans
 
 if __name__ == '__main__':
     start_num = int(input())
